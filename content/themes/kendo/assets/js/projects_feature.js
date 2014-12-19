0fc0d81e7f5e28854cd1e48c37542cf6081ac202
Willie Koomson <willie.koomson@gmail.com>
2014-12-18 19:16:23 -0500

    Added two of three snaps

--- content/themes/kendo/assets/js/projects_feature.js
+++ content/themes/kendo/assets/js/projects_feature.js
@@ -1,41 +1,45 @@
-var projects = [
-	{
-		"project_title": "The Undergraduate Times",
-		"project_description": "First online news publication run by undergraduates, for undergraduates",
-		"project_link": "http://ugtimes.com",
-		"project_role": "Android Developer",
-		"background_color": "#242524",
-		"accent_color": "#046D3C",
-		"text_color": "#FFFFFF",
-		"font": "Georgia",
-		"svg_source": ""
-	},
-	{
-		"project_title": "PackItTrackIt",
-		"project_description": "Track your packages from the web and mobile, with notifications and automatic adding",
-		"project_link": "https://github.com/wkoomson/PackItTrackIt",
-		"project_role": "Lead Developer (Android/Web)",
-		"background_color": "#FFC107",
-		"accent_color": "#FF6F00",
-		"text_color": "#263238",
-		"font": "Roboto",
-		"svg_source": ""
-	},
-	{
-		"project_title": "Medi",
-		"project_description": "A groundbreaking interface for patient and healthcare provider interaction, on web and mobile",
-		"project_link": "https://github.com/wkoomson/Medi",
-		"project_role": "Lead Developer (Android/Web)",
-		"background_color": "#03A9F4",
-		"accent_color": "#4A148C",
-		"text_color": "#FFFFFF",
-		"font": "Helvetica Neue",
-		"svg_source": ""
-	}
-]
+$(function () {
+	var projects = [
+		{
+			"project_title": "The Undergraduate Times",
+			"project_description": "First online news publication run by undergraduates, for undergraduates",
+			"project_link": "http://ugtimes.com",
+			"project_role": "Android Developer",
+			"background_color": "#242524",
+			"accent_color": "#046D3C",
+			"text_color": "#FFFFFF",
+			"font": "Georgia",
+			"svg_source": "assets/images/ugtimes_project_snap.svg"
+		},
+		{
+			"project_title": "PackItTrackIt",
+			"project_description": "Track your packages from the web and mobile, with notifications and automatic adding",
+			"project_link": "https://github.com/wkoomson/PackItTrackIt",
+			"project_role": "Lead Developer (Android/Web)",
+			"background_color": "#FFC107",
+			"accent_color": "#FF6F00",
+			"text_color": "#263238",
+			"font": "Roboto",
+			"svg_source": "assets/images/packittrackit_project_snap.svg"
+		},
+		{
+			"project_title": "Medi",
+			"project_description": "A groundbreaking interface for patient and healthcare provider interaction, on web and mobile",
+			"project_link": "https://github.com/wkoomson/Medi",
+			"project_role": "Lead Developer (Android/Web)",
+			"background_color": "#03A9F4",
+			"accent_color": "#4A148C",
+			"text_color": "#FFFFFF",
+			"font": "Open Sans",
+			"svg_source": ""
+		}
+	]
 
-function projectFeature (data) {
-	for (var i = data.length - 1; i >= 0; i--) {
+	var counter = 0; 
+
+	function projectFeature (e, data, i) {
+		e.preventDefault();
+
 		$("section.projects_feature").velocity({
 			backgroundColor: data[i].background_color
 		},{
@@ -63,5 +67,29 @@ function projectFeature (data) {
 				"color": data[i].text_color,
 				"font-family": data[i].font
 			});
+		$("h2.projects_feature.section_title")
+			.css({
+				"color": data[i].text_color,
+				"font-family": data[i].font
+			});
+
+		$("#svg_element.projects_feature")
+			.attr("src", data[i].svg_source);
+
+
+		i++;
+
+		if (i == data.length) {
+			i = 0;
+		}
+		setTimeout(function () {
+			$.event.trigger("slideChange", [data, i]);
+		}, 5000);
+
 	};
-}
+
+	$(document).on("slideChange", projectFeature);
+
+	$.event.trigger("slideChange", [projects, counter]);
+
+});
